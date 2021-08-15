76d75e4bb73295ed7855db17d71933fa46211201
Binod Nagarkoti <binod1365@gmail.com>
2021-08-16 00:22:50 +0545

    first commit

---
+++ src/comingSoon.js
@@ -0,0 +1,25 @@
+/** @format */
+
+import React from 'react';
+
+export default function Comingsoon() {
+    return (
+        <main className='wrapper'>
+            <h1>
+                coming soon<span className='dot'>.</span>
+            </h1>
+            <p>Binod Nagarkoti</p>
+            <div className='icons'>
+                <a href='https://www.linkedin.com/in/binod-nagarkoti-496245128/'>
+                    <i className='fa fa-linkedin'></i>
+                </a>
+                <a href='https://github.com/BinodNagarkoti'>
+                    <i className='fa fa-github'></i>
+                </a>
+                <a href='mailto:binod1365@gmail.com?subject=contact%20us '>
+                    <i className='fa fa-paper-plane'></i>
+                </a>
+            </div>
+        </main>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Comingsoon from './comingSoon';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Comingsoon />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
